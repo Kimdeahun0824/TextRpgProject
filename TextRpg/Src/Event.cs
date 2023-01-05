66f2de535bbe496cfadf4abc31bd3cd09e207d0b
kimdeahun0824 <garamei72@gmail.com>
2023-01-05 22:28:20 +0900

    [dhkim] 커밋 오류로 인한 재 커밋

--- TextRpg/Src/Event.cs
+++ TextRpg/Src/Event.cs
@@ -11,6 +11,7 @@ namespace TextRpg.Src
     {
         private string mName;
         private string mContent;
+
         private List<string> mOptional;
 
         private bool mIs_Next_Content;
@@ -20,15 +21,58 @@ namespace TextRpg.Src
         private int mRewardGold;
         private int mRewardExp;
 
-        private int mDelay; 
-        
+        private int mDelay;
+
         public Event()
         {
 
         }
 
-
-
+        public string Name
+        {
+            get { return mName; }
+            private set { mName = value; }
+        }
+        public string Content
+        {
+            get { return mContent; }
+            private set { mContent = value; }
+        }
+        public List<string> Optional
+        {
+            get { return mOptional; }
+            private set { mOptional = value; }
+        }
+        public bool Is_Next_Content
+        {
+            get { return mIs_Next_Content; }
+            private set { mIs_Next_Content = value; }
+        }
+        public List<string> NextContentKey
+        {
+            get { return mNextContentKey; }
+            private set { mNextContentKey = value; }
+        }
+        public Item RewardItem
+        {
+            get { return mRewardItem; }
+            private set { mRewardItem = value; }
+        }
+        public int RewardGold
+        {
+            get { return mRewardGold; }
+            private set { mRewardGold = value; }
+        }
+        public int RewardExp
+        {
+            get { return mRewardExp; }
+            private set { mRewardExp = value; }
+        }
+        public int Delay
+        {
+            get { return mDelay; }
+            private set { mDelay = value; }
+        }
 
     }
 }
